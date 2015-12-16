cfb12806bb12a7c22a268dfa6f2996b6513fe002
root <root@ip-172-31-20-120.eu-central-1.compute.internal>
2015-12-16 09:32:29 +0000

    Fix aws version

--- backup.py
+++ backup.py
@@ -6,10 +6,11 @@ import json
 import time
 from datetime import datetime
 
+AWS = '/usr/local/bin/aws'
 
 INSTANCE_ID = os.popen('ec2metadata | grep -Po "instance-id:\s*\K.+"').read()
 
-res = os.popen('aws ec2 describe-instances --instance-ids {}'.format(INSTANCE_ID)).read()
+res = os.popen('{} ec2 describe-instances --instance-ids {}'.format(AWS, INSTANCE_ID)).read()
 instance = json.loads(res)
 VOLUME_ID = instance['Reservations'][0]['Instances'][0]['BlockDeviceMappings'][0]['Ebs']['VolumeId']
 
@@ -17,13 +18,13 @@ os.system('sudo /edx/bin/supervisorctl stop all')
 os.system('sudo service mongod stop')
 os.system('sudo service mysql stop')
 
-res = os.popen('aws ec2 create-snapshot --volume-id {} --description "EDX `date +%Y-%m-%d` backup"'.format(VOLUME_ID)).read()
+res = os.popen('{} ec2 create-snapshot --volume-id {} --description "EDX `date +%Y-%m-%d` backup"'.format(AWS, VOLUME_ID)).read()
 snapshot = json.loads(res)
 start_time = time.time()
 snapshot_id = snapshot['SnapshotId']
 
 while True:
-    res = os.popen('aws ec2 describe-snapshots --snapshot-ids "{}"'.format(snapshot_id)).read()
+    res = os.popen('{} ec2 describe-snapshots --snapshot-ids "{}"'.format(AWS, snapshot_id)).read()
     snapshot = json.loads(res)
 
     state = snapshot['Snapshots'][0]['State']
@@ -38,10 +39,10 @@ os.system('sudo service mysql start')
 os.system('sudo service mongod start')
 os.system('sudo /edx/bin/supervisorctl start all')
 
-res = os.popen('aws ec2 describe-snapshots --filters Name=description,Values=EDX*backup Name=status,Values=completed').read()
+res = os.popen('{} ec2 describe-snapshots --filters Name=description,Values=EDX*backup Name=status,Values=completed'.format(AWS)).read()
 snapshots = json.loads(res)['Snapshots']
 snapshots.sort(key=lambda x: x['StartTime'])
 
 for snap in snapshots[:-7]:
     print 'Delete shapshot "{}" ...'.format(snap['SnapshotId'])
-    os.system('aws ec2 delete-snapshot --snapshot-id {}'.format(snap['SnapshotId']))
+    os.system('{} ec2 delete-snapshot --snapshot-id {}'.format(AWS, snap['SnapshotId']))
